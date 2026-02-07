7e3f61db2882f5c88d4a552515503b739337b2e2
Mxaimilian Plangg Filho <maximilianplanggfilho@gmail.com>
2026-02-07 17:09:15 -0300

    novo poder

---
+++ java.js
@@ -0,0 +1,5 @@
+function ativarPowerUp() {
+    doubleShotActive = true;
+    powerupTimer = 1200; // Alterado para 20 segundos (60fps * 20)
+    document.getElementById('powerup-timer').style.display = 'block';
+}
